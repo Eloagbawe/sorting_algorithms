08242d300aebd147c8bdebd223db7b30c0c47668
Eloagbawe <eloagbawe@gmail.com>
2022-08-16 00:17:35 +0000

    Quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,80 @@
+#include "sort.h"
+/**
+ * swap - swaps two array elements
+ * @a: first element
+ * @b: second element
+ * Return: void
+*/
+void swap(int *a, int *b)
+{
+	int temp1, temp2;
+
+	temp1 = *a;
+	temp2 = *b;
+	*a = temp2;
+	*b = temp1;
+}
+
+/**
+ * partition - function to divide the array
+ * @array: array
+ * @low: left part of the pivot
+ * @high: right part of the pivot
+ * @size: size of the array
+ * Return: pivot index
+*/
+
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot, i, j;
+
+	pivot = array[high];
+	i = low;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			swap(&(array[i]), &(array[j]));
+			i++;
+		}
+	}
+	swap(&(array[i]), &(array[high]));
+	print_array(array, size);
+	return (i);
+}
+
+/**
+ * sort - sorts an array
+ * @array: array to be sorted
+ * @low: left part of pivot
+ * @high: right part of pivot
+ * @size: size of the array
+ * Return: void
+*/
+
+void sort(int *array, int low, int high, size_t size)
+{
+	int i;
+
+	if (low >= high || low < 0)
+		return;
+	i = partition(array, low, high, size);
+	sort(array, low, i - 1, size);
+	sort(array, i + 1, high, size);
+}
+
+/**
+ * quick_sort - quick sort implementation on an array
+ * @array: array to be sorted
+ * @size: size of the array
+ * Return: void
+*/
+
+void quick_sort(int *array, size_t size)
+{
+	if (size > 1)
+	{
+		sort(array, 0, size - 1, size);
+	}
+}
